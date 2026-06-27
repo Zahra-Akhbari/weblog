dd6786053a4ddc342c3acb962968fa3a17a44874
Nazanin <nz.akhbari@gmail.com>
2026-06-27 18:47:48 +0330

    add comment in post_detail and send comment to DB

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,10 @@
+from django.contrib.auth.models import User
 from django.shortcuts import render
 from django.shortcuts import get_object_or_404
+from django.shortcuts import redirect
 from .models import Post
+
+from .forms import CommentForm
 
 
 def home(request):
@@ -23,9 +27,34 @@ def post_detail(request, slug):
         slug=slug,
         status='published'
     )
+    if request.method == 'POST':
+        # فقط داده‌های کاربر را وارد شیء فرم کرده‌ایم.
+        form=CommentForm(request.POST)
+
+        # قسمت اعتبار سنجی
+        if form.is_valid():
+
+            # چون دو فیلد اجباری در مدل کامنت هنوز مقدار ندارن
+            # پس یک شی میسازیم و میگیم کامنت بساز ولی ذخیره نکن در DB
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.author = User.objects.first()
+            comment.save()
+
+            return redirect(
+                'post_detail',
+                slug=post.slug
+            )
+
+
+    else:
+        # یک فرم خالی بساز.
+        form=CommentForm()
+
 
     context = {
-        'post': post
+        'post': post,
+        'form': form,
     }
 
     return render(
