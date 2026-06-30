343bb5545bce780df1edac5b7ef1c083f48c8a38
Nazanin <nz.akhbari@gmail.com>
2026-06-30 13:21:45 +0330

    login.py and menu in base page

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from .views import home,post_detail
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('home/', home, name='home'),
     path('<slug:slug>/', post_detail, name='post_detail'),
+    path("login/",auth_views.LoginView.as_view(template_name="blog/login.html"),name="login"),
+    path("logout/",auth_views.LogoutView.as_view(),name="logout"),
 ]
