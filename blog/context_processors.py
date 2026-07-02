7b09d7ddeeeccb0c9adcd0e6fdbb658d588f13d3
Nazanin <nz.akhbari@gmail.com>
2026-07-02 18:06:17 +0330

    add categories,latest Posts,Tags into sidebar and connected to DB. better navbar design

--- blog/context_processors.py
+++ blog/context_processors.py
@@ -1,11 +1,11 @@
 from .models import Category, Tag, Post
-
+from django.db.models import Count
 
 def sidebar_data(request):
     return {
-        "categories": Category.objects.all(),
+        "categories": Category.objects.annotate(post_count=Count("posts")),
         "tags": Tag.objects.all(),
-        "latest_posts": Post.objects.filter(
-            status="published"
-        ).order_by("-created_at")[:5],
+        "latest_posts": Post.objects.filter(status="published").order_by("-created_at")[:5],
     }
+
+
