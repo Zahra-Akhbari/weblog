24bdd1fb9eb5eaaf95b09ee43e241c24c106c4e1
Nazanin <nz.akhbari@gmail.com>
2026-07-02 10:56:42 +0330

    changing appearance with Bootstrap

--- config/settings.py
+++ config/settings.py
@@ -104,7 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/6.0/topics/i18n/
 
-LANGUAGE_CODE = "fa-ir"
+LANGUAGE_CODE = "en-us"
 
 TIME_ZONE = "Asia/Tehran"
 
